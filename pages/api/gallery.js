1b9ce43ad37b3931d48554505c26be4917f3b605
RogueWild <0.dreamer.009@gmail.com>
2021-03-23 20:01:42 +0300

    Gallery thumb update

--- pages/api/gallery.js
+++ pages/api/gallery.js
@@ -1,51 +1,51 @@
 export const artImages = [
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/033/066/658/large/mike-u-space-in-my-hex.jpg?1608293227',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/033/066/658/large/mike-u-space-in-my-hex.jpg?1608293227',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/033/066/658/20201218060707/smaller_square/mike-u-space-in-my-hex.jpg?1608293227',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/032/186/326/large/mike-u-cyberpunk.jpg?1605713631',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/032/186/326/large/mike-u-cyberpunk.jpg?1605713631',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/032/186/326/smaller_square/mike-u-cyberpunk.jpg?1605713631',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/010/371/050/large/mike-u-everyday-06-civil-war.jpg?1524066991',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/010/371/050/large/mike-u-everyday-06-civil-war.jpg?1524066991',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/010/371/050/smaller_square/mike-u-everyday-06-civil-war.jpg?1524066991',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/027/333/660/large/mike-u-pillars.jpg?1591232524',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/027/333/660/large/mike-u-pillars.jpg?1591232524',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/027/333/660/20200603200204/smaller_square/mike-u-pillars.jpg?1591232524',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/021/445/306/large/mike-u-skull.jpg?1571716740',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/021/445/306/large/mike-u-skull.jpg?1571716740',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/021/445/306/smaller_square/mike-u-skull.jpg?1571716740',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/018/914/180/large/mike-u-somewhere.jpg?1561225308',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/018/914/180/large/mike-u-somewhere.jpg?1561225308',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/018/914/180/20190622124148/smaller_square/mike-u-somewhere.jpg?1561225308',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/020/111/762/large/mike-u-skull-2.jpg?1566408873',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/020/111/762/large/mike-u-skull-2.jpg?1566408873',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/020/111/762/smaller_square/mike-u-skull-2.jpg?1566408873',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/015/076/626/large/mike-u-helmet-concept.jpg?1546968158',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/015/076/626/large/mike-u-helmet-concept.jpg?1546968158',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/015/076/626/20190108112238/smaller_square/mike-u-helmet-concept.jpg?1546968158',
     },
     {
         original: 'https://cdnb.artstation.com/p/assets/images/images/012/794/995/large/mike-u-vagarypt.jpg?1536574335',
-        thumbnail: 'https://cdnb.artstation.com/p/assets/images/images/012/794/995/large/mike-u-vagarypt.jpg?1536574335',
+        thumbnail: 'https://cdnb.artstation.com/p/assets/images/images/012/794/995/smaller_square/mike-u-vagarypt.jpg?1536574335',
     },
     {
         original: 'https://cdna.artstation.com/p/assets/images/images/011/967/672/large/mike-u-everyday-09.jpg?1532361948',
-        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/011/967/672/large/mike-u-everyday-09.jpg?1532361948',
+        thumbnail: 'https://cdna.artstation.com/p/assets/images/images/011/967/672/smaller_square/mike-u-everyday-09.jpg?1532361948',
     },
     {
         original: 'https://cdnb.artstation.com/p/assets/images/images/010/785/973/large/mike-u-another-day.jpg?1526236150',
-        thumbnail: 'https://cdnb.artstation.com/p/assets/images/images/010/785/973/large/mike-u-another-day.jpg?1526236150',
+        thumbnail: 'https://cdnb.artstation.com/p/assets/images/images/010/785/973/smaller_square/mike-u-another-day.jpg?1526236150',
     },
     {
         original: 'https://cdnb.artstation.com/p/assets/images/images/010/583/557/large/mike-u-everyday-01.jpg?1525190650',
-        thumbnail: 'https://cdnb.artstation.com/p/assets/images/images/010/583/557/large/mike-u-everyday-01.jpg?1525190650',
+        thumbnail: 'https://cdnb.artstation.com/p/assets/images/images/010/583/557/20180501110409/smaller_square/mike-u-everyday-01.jpg?1525190650',
     },
 ];
 
